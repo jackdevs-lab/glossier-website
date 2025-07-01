42bf0bc3a87538d2a75c3473a9aa9f1edb4d2741
Ndegwa Jackson <128777440+jackdevs-lab@users.noreply.github.com>
2025-07-01 09:09:42 +0300

    updates

--- public/js/products.js
+++ public/js/products.js
@@ -1,4 +1,7 @@
 // /js/products.js
+const API_BASE_URL = process.env.NODE_ENV === 'development'
+  ? 'http://localhost:3000'
+  : 'https://glossier-website.vercel.app';
 async function getProductsByCategory(category) {
     try {
         const response = await fetch(`http://localhost:3000/api/products/${category}`);
